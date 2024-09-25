ac13ae426a8284bfc1c563abc968700f61636cf5
Mathias Forsbrg <mathias@forsberg.cc>
2024-09-25 17:01:31 +0200

    Added Keyvault and JWT

---
+++ training-log.Server/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using training_log.Server.Interfaces;
+
+namespace training_log.Server.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateJwtToken(string userId)
+        {
+            var jwtKey = _configuration["JWT:JwtSecret"];
+            var jwtIssuer = _configuration["JWT:Issuer"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtIssuer,
+                audience: jwtIssuer,
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials
+
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
